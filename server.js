f84b2a620f853c05c175a1ce5cc652ecb1af212d
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-16 15:36:17 -0400

    created POST routes for html

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-// HTML routes
+// HTML GET routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -18,3 +18,25 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes/html"));
 });
+
+//HTML POST routes
+app.post("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+        if(error) {
+            return console.log(error)
+        }
+        notes = JSON.parse(notes)
+
+        var id = notes[notes.length - 1].id + 1
+        var newNote = {title: req.body.title, text: req.body.text, id: id }
+        var activeNote = notes.concat(newNote)
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+            if (error) {
+                return error
+            }
+            console.log(activeNote)
+            res.json(activeNote);
+        })
+    })
+});s
