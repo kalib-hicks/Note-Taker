f92562517021e82e3d076bcd301995fce56a7beb
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-16 15:27:10 -0400

    added index.html route

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+// HTML routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
