508d375c3e502811a287b5bdf6b9c49e815d0c10
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-16 15:28:38 -0400

    added notes.html route

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes/html"));
+});
